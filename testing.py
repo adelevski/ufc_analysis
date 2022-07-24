3075ec1fe4f4b4a13cf856fb5bc06c7910c38835
Atanas Delevski <adelevski@gmail.com>
2022-07-24 18:52:10 -0500

    init repoo

---
+++ testing.py
@@ -0,0 +1,23 @@
+# Import statements
+import pandas as pd
+
+
+# Reading in csv file and turning it into a Pandas DataFrame
+df = pd.read_csv("data/data.csv")
+
+
+# Printing all the columns 
+for column in df.columns:
+    print(column)
+
+
+# Printing all columns about the Red fighter
+for column in df.columns:
+    if column.startswith("R_"):
+        print(column)
+
+
+# Printing general columns (not about either fighter)
+for column in df.columns:
+    if not column.startswith("R_") and not column.startswith("B_"):
+        print(column)
